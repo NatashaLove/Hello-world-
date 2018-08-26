47b4ea8c6e5e3d8715fa8c98ef16dc27c6757ddc
Natasha S Love <debora@inbox.ru>
2018-08-25 23:51:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nLove_inclass01
---
+++ nLove_inclass01.cs
@@ -0,0 +1,19 @@
+/* In -class Assignment 01
+* By: Natalia Love */
+
+using System;
+namespace nlove_inclass01 {
+	class Program {
+		public static void Main() {
+			// clear the console
+			Console.Clear();
+			
+			//display "Hello World!"
+			Console.WriteLine("Hello World!");
+			
+			// wait for user to respond
+			Console.WriteLine("Press any key.");
+			Console.ReadKey();
+		}
+	}
+}
